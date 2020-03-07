8800cc23eef22e57bc1c0b839c9143dbfc2cdf55
Mike Trame <mtrame@umich.edu>
2020-03-06 20:06:53 -1000

    Added setup script for setting spotipy environment variables.

---
+++ bin/spotipy_env_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Setup script for setting spotipy client env variables.
+# USAGE: source bin/spotipy_env_setup.sh
+
+echo "source bin/spotipy_env_setup.sh"
+export SPOTIPY_CLIENT_ID='700ea8de67bd443fb29d777f3a5c4b94'
+export SPOTIPY_CLIENT_SECRET='cbff50d47f8e4387abcea472861232fc'
