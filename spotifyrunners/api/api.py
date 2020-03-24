f49248f24135b03c650248d56a2b619da4e87a2c
Conor Rafferty <craffer@umich.edu>
2020-03-24 15:04:21 -0400

    Convert API route to POST request to create a playlist
    Co-authored-by: Jonah Rosenblum <jonaherosenblum@gmail.com>
    Co-authored-by: Jack Shelata <jack@jackshelata.com>

--- spotifyrunners/api/api.py
+++ spotifyrunners/api/api.py
@@ -9,59 +9,65 @@ import spotifyrunners
 SCOPE = "user-library-read playlist-modify-public"
 
 
-@spotifyrunners.app.route("/api/v1/", methods=["GET"])
+@spotifyrunners.app.route("/api/v1/create", methods=["GET", "POST"])
 def get_tracks():
     """Return up to 50 tracks from users library that match a given bpm +/- 5 bpm."""
-    username = flask.request.args.get(
-        "username", default="d109pti75pqyij348fp3v3ho9", type=str
-    )
-    bpm = flask.request.args.get("bpm", default=125, type=int)
-    print("Username:", username)
-    print("BPM:", bpm)
-    print("Welcome to Spotify Running! Please log in using your browser.")
-    token = spotipy.util.prompt_for_user_token(username, SCOPE)
+    context = {}
+    if flask.request.method == "POST":
+        request_info = flask.request.get_json()
+        username = (
+            request_info["username"]
+            if "username" in request_info
+            else "d109pti75pqyij348fp3v3ho9"
+        )
+        bpm = request_info["bpm"] if "bpm" in request_info else 125
 
-    if token:
-        sp = spotipy.Spotify(auth=token)
+        print("Welcome to Spotify Running! Please log in using your browser.")
+        token = spotipy.util.prompt_for_user_token(username, SCOPE)
 
-        # get the IDs of all saved tracks for the logged in user
-        print("Analyzing your library...")
-        ids = []
-        current_results = sp.current_user_saved_tracks(limit=50)
-        while current_results:
-            current_results = sp.next(current_results)
-            if current_results:
-                ids.append([item["track"]["id"] for item in current_results["items"]])
+        print("Username:", username)
+        print("BPM:", bpm)
 
-        # get the audio features (including BPM) for each saved track
-        features_list = []
-        for id_group in ids:
-            features_list.append(sp.audio_features(id_group))
+        if token:
+            sp = spotipy.Spotify(auth=token)
 
-        print("Finding songs that match your BPM...")
-        bpm_matches = []
-        for lst in features_list:
-            for track in lst:
-                # if this is within 5 of our desired BPM on either side, we grab it
-                if -5 <= track["tempo"] - bpm <= 5:
-                    bpm_matches.append(track["uri"])
-        context = {"tracks": bpm_matches}
-        if bpm_matches:
-            print("Creating a playlist for all the songs we've found!")
-            new_playlist = sp.user_playlist_create(username, "Spotify Running")
-            bpm_chunks = [
-                bpm_matches[x : x + 100] for x in range(0, len(bpm_matches), 100)
-            ]
-            for chunk in bpm_chunks:
-                sp.user_playlist_add_tracks(username, new_playlist["id"], chunk)
-            print("All done! Check out your new Spotify Running playlist.")
-        else:
-            print("No songs match. Sorry :/")
+            # get the IDs of all saved tracks for the logged in user
+            print("Analyzing your library...")
+            ids = []
+            current_results = sp.current_user_saved_tracks(limit=50)
+            while current_results:
+                current_results = sp.next(current_results)
+                if current_results:
+                    ids.append(
+                        [item["track"]["id"] for item in current_results["items"]]
+                    )
 
-        return flask.jsonify(**context)
+            # get the audio features (including BPM) for each saved track
+            features_list = []
+            for id_group in ids:
+                features_list.append(sp.audio_features(id_group))
 
-    print("Can't get token for", username)
-    context = {"message": "Bad Request", "info": "Can't get token", "status_code": 400}
-    resp = flask.jsonify(**context)
-    resp.status_code = 400
-    return resp
+            print("Finding songs that match your BPM...")
+            bpm_matches = []
+            for lst in features_list:
+                for track in lst:
+                    # if this is within 5 of our desired BPM on either side, we grab it
+                    if -5 <= track["tempo"] - bpm <= 5:
+                        bpm_matches.append(track["uri"])
+            if bpm_matches:
+                print("Creating a playlist for all the songs we've found!")
+                new_playlist = sp.user_playlist_create(username, "Spotify Running")
+                bpm_chunks = [
+                    bpm_matches[x : x + 100] for x in range(0, len(bpm_matches), 100)
+                ]
+                for chunk in bpm_chunks:
+                    sp.user_playlist_add_tracks(username, new_playlist["id"], chunk)
+                print("All done! Check out your new Spotify Running playlist.")
+                context = {"status": "created"}
+            else:
+                print("No songs match. Sorry :/")
+                context = {"status": "no matches"}
+            return flask.jsonify(**context)
+
+    # if it's a GET request, just redirect them to the index page
+    return flask.redirect(flask.url_for("show_index"))
