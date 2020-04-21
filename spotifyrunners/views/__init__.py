a346e9ac370e6e01851098708c14fda6af9d5dd2
Jack Shelata <jshelata@umich.edu>
2020-04-20 20:27:01 -0400

    Add very simple BPM calculator tool

--- spotifyrunners/views/__init__.py
+++ spotifyrunners/views/__init__.py
@@ -1,3 +1,4 @@
 """Views for spotifyrunners, just one for now."""
 from spotifyrunners.views.index import show_index
 from spotifyrunners.views.player import show_player
+from spotifyrunners.views.bpm import show_bpm
